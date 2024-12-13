d610ea25444ef2e9e243d4284fb1ddea2e61dbd1
Ji-eun Yang <jidak1165@gmail.com>
2024-12-13 10:39:40 +0900

    initial setting

---
+++ src/main/java/com/omnm/hanasset/HanassetApplication.java
@@ -0,0 +1,13 @@
+package com.omnm.hanasset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanassetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanassetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnm/hanasset/HanassetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnm.hanasset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanassetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
