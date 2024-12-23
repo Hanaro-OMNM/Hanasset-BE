f3be7af93fb64a7e6b978e4a840f5f3c4fd57ecd
insoo00 <insoosos1234@gmail.com>
2024-12-23 17:43:18 +0900

    [proj-119] feat: 매물 시세 api 구현

--- src/main/java/com/omnm/hanasset/global/exception/code/ErrorCode.java
+++ src/main/java/com/omnm/hanasset/global/exception/code/ErrorCode.java
@@ -10,7 +10,10 @@ public enum ErrorCode {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 엔드포인트입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
+
+    // RealEstate ErrorCode
+    REAL_ESTATE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 부동산이 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
