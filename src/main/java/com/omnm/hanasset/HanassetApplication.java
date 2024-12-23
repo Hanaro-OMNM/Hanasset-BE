2c519518950cd885978892a6617dd104dc08dce0
Ji-eun Yang <jidak1165@gmail.com>
2024-12-23 19:24:24 +0900

    [proj-105] fix: rebase로 잘못된 파일 수정

--- src/main/java/com/omnm/hanasset/HanassetApplication.java
+++ src/main/java/com/omnm/hanasset/HanassetApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HanassetApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(HanassetApplication.class, args);
+		SpringApplication. run(HanassetApplication.class, args);
 	}
 }
