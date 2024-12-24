95b88becf77eedf3bdd960c6678a39bf9d3493ed
kkx7787 <kkx7787@naver.com>
2024-12-24 14:04:13 +0900

    [proj-105] hot-fix: 특정 엔드포인트 허용

--- src/main/java/com/omnm/hanasset/global/config/security/SecurityConfig.java
+++ src/main/java/com/omnm/hanasset/global/config/security/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                                 .requestMatchers("/users/birth/**").permitAll()
                                 .requestMatchers("/users/logout/**").permitAll()
                                 .requestMatchers("/users/me/**").authenticated()
+                                .requestMatchers("/markers/**").permitAll()
+                                .requestMatchers("/real-estates/**").permitAll()
                                 .requestMatchers("/swagger", "/swagger-ui.html").permitAll()
                                 .requestMatchers("/swagger-resources/**").permitAll()
                                 .requestMatchers("/swagger-ui/**", "/api-docs/**").permitAll()
