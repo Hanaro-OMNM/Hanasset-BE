3b1b0fed839a366dc306095b46c6ff2c24234753
Ji-eun Yang <jidak1165@gmail.com>
2024-12-24 10:17:35 +0900

    [proj-105] fix: 필요없는 import 제거

--- src/main/java/com/omnm/hanasset/global/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/omnm/hanasset/global/config/security/JwtAuthenticationFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
