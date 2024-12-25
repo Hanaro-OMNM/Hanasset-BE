efab6ca1f1ec46490b419580d38f11b74242252e
leedy903 <leedy903@naver.com>
2024-12-25 20:00:21 +0900

    [proj-104] feat: 로그인된 유저만 필터링된 대출 상품을 볼 수 있도록 변경

--- src/main/java/com/omnm/hanasset/global/config/security/SecurityConfig.java
+++ src/main/java/com/omnm/hanasset/global/config/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                                 .requestMatchers("/users/birth/**").permitAll()
                                 .requestMatchers("/users/logout/**").permitAll()
                                 .requestMatchers("/users/me/**").authenticated()
-                                .requestMatchers("/loan/**").permitAll()
+                                .requestMatchers("/loan/**").authenticated()
                                 .requestMatchers("/markers/**").permitAll()
                                 .requestMatchers("/real-estates/**").permitAll()
                                 .requestMatchers("/users/bookmarks/**").authenticated()
