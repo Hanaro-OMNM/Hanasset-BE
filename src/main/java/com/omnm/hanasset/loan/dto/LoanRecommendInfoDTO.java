5d8c7399cc5a78d288aa58c6e50568aca44ff50b
leedy903 <leedy903@naver.com>
2024-12-25 17:36:27 +0900

    [proj-104] feat: 추천 대출 상품 응답에 매물 정보 추가

--- src/main/java/com/omnm/hanasset/loan/dto/LoanRecommendInfoDTO.java
+++ src/main/java/com/omnm/hanasset/loan/dto/LoanRecommendInfoDTO.java
@@ -1,5 +1,6 @@
 package com.omnm.hanasset.loan.dto;
 
+import com.omnm.hanasset.realEstate.dto.RealEstateInfoResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
@@ -11,10 +12,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoanRecommendInfoDTO {
-    /**
-     * TODO RealEstateDTO 추가
-     * RealEstateDTO realEstateDTO;
-     */
+    @Schema(description = "매물 데이터 정보")
+    RealEstateInfoResponse realEstateInfoResponse;
     @Schema(description = "하나은행 대출 상품 추천 목록")
     List<LoanInfoDTO> hanaLoans;
     @Schema(description = "버팀목 대출 상품 추천 목록")
