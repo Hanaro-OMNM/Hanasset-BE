c331c78771f4914aa2bf25ecdce11f49dfb4be99
leedy903 <leedy903@naver.com>
2024-12-25 17:35:22 +0900

    [proj-104] feat: loan 필터 무시 조건 추가

--- src/main/java/com/omnm/hanasset/global/config/security/SecurityConfig.java
+++ src/main/java/com/omnm/hanasset/global/config/security/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                                 .requestMatchers("/users/birth/**").permitAll()
                                 .requestMatchers("/users/logout/**").permitAll()
                                 .requestMatchers("/users/me/**").authenticated()
+                                .requestMatchers("/loan/**").permitAll()
                                 .requestMatchers("/markers/**").permitAll()
                                 .requestMatchers("/real-estates/**").permitAll()
                                 .requestMatchers("/users/bookmarks/**").authenticated()
