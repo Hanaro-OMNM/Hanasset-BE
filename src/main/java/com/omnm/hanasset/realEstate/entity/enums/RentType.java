fb4e3250477caa869a9f9bef1b9563c812d8b95f
leedy903 <leedy903@naver.com>
2024-12-25 20:26:33 +0900

    [proj-104] feat: 사용하지 않는 enum 제거

--- src/main/java/com/omnm/hanasset/realEstate/entity/enums/RentType.java
+++
@@ -1,6 +0,0 @@
-package com.omnm.hanasset.realEstate.entity.enums;
-
-public enum RentType {
-    JEONSE,
-    WOLSE
-}
--- src/main/java/com/omnm/hanasset/realEstate/entity/enums/Type.java
+++
@@ -1,6 +0,0 @@
-package com.omnm.hanasset.realEstate.entity.enums;
-
-public enum Type {
-    APARTMENT,
-    VILLA
-}
