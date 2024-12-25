e24a36cb62d922d5a54be20b28832abee96c5ee4
leedy903 <leedy903@naver.com>
2024-12-25 20:00:06 +0900

    [proj-104] feat: 로그인된 유저만 필터링된 대출 상품을 볼 수 있도록 변경

--- src/main/java/com/omnm/hanasset/loan/controller/LoanController.java
+++ src/main/java/com/omnm/hanasset/loan/controller/LoanController.java
@@ -1,6 +1,7 @@
 package com.omnm.hanasset.loan.controller;
 
 import com.omnm.hanasset.global.common.ApiResponseEntity;
+import com.omnm.hanasset.global.dto.UserDetailsDTO;
 import com.omnm.hanasset.loan.dto.LoanDetailResponse;
 import com.omnm.hanasset.loan.dto.LoanResponse;
 import com.omnm.hanasset.loan.service.LoanService;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,9 +24,8 @@ public class LoanController {
     @Operation(summary = "대출 추천", description = "손님 정보와 매물 정보로 대출을 추천한다.")
     @ApiResponse(responseCode = "200", description = "대출 상품 추천 성공")
     @GetMapping
-    public ApiResponseEntity<LoanResponse> getRecommendLoans(@RequestParam List<Long> realEstateIds) {
-        // userId = 1L
-        LoanResponse recommendLoans = loanService.getRecommendLoans(1L, realEstateIds);
+    public ApiResponseEntity<LoanResponse> getRecommendLoans(@AuthenticationPrincipal UserDetailsDTO userDetailsDTO, @RequestParam List<Long> realEstateIds) {
+        LoanResponse recommendLoans = loanService.getRecommendLoans(userDetailsDTO.getId(), realEstateIds);
         return ApiResponseEntity.ok("대출 상품 추천 성공", recommendLoans);
     }
 
