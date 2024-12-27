3f8c2899335f65b093b8a2ccacb5cf6a8d675a08
leedy903 <leedy903@naver.com>
2024-12-27 15:28:54 +0900

    [proj-134] feat: 손님과 상담원이 상담 시에 접근하는 손님 정보 API 추가

--- src/main/java/com/omnm/hanasset/loan/controller/LoanController.java
+++ src/main/java/com/omnm/hanasset/loan/controller/LoanController.java
@@ -1,6 +1,8 @@
 package com.omnm.hanasset.loan.controller;
 
+import com.omnm.hanasset.chat.service.ChatRoomService;
 import com.omnm.hanasset.global.common.ApiResponseEntity;
+import com.omnm.hanasset.global.dto.ConsultantDetailsDTO;
 import com.omnm.hanasset.global.dto.UserDetailsDTO;
 import com.omnm.hanasset.loan.dto.LoanDetailResponse;
 import com.omnm.hanasset.loan.dto.LoanResponse;
@@ -20,6 +22,23 @@ import java.util.List;
 @RequestMapping("/loan")
 public class LoanController {
     private final LoanService loanService;
+
+    @Operation(summary = "상담 시 손님이 요청한 손님 및 추천 대출 정보", description = "손님 정보와 등록한 매물 정보로 상담 시 추천 대출 상품 목록을 보여준다.")
+    @ApiResponse(responseCode = "200", description = "상담 시 대출 상품 추천 성공")
+    @GetMapping("/consulting")
+    public ApiResponseEntity<LoanResponse> getConsultingRecommendLoansByGuest(@AuthenticationPrincipal UserDetailsDTO userDetailsDTO, @RequestParam String chatroomId) {
+        LoanResponse recommendLoans = loanService.getConsultingRecommendLoans(userDetailsDTO.getId(), chatroomId);
+        return ApiResponseEntity.ok("상담 대출 상품 추천 성공", recommendLoans);
+    }
+
+    @Operation(summary = "상담 시 상담원이 요청한 손님 및 추천 대출 정보", description = "손님 정보와 등록한 매물 정보로 상담 시 추천 대출 상품 목록을 보여준다.")
+    @ApiResponse(responseCode = "200", description = "상담 시 대출 상품 추천 성공")
+    @GetMapping("/consultant")
+    public ApiResponseEntity<LoanResponse> getConsultingRecommendLoansByConsultant(@AuthenticationPrincipal ConsultantDetailsDTO consultantDetailsDTO, @RequestParam String chatroomId, @RequestParam Long userId) {
+        LoanResponse recommendLoans = loanService.getConsultingRecommendLoans(userId, chatroomId);
+        return ApiResponseEntity.ok("상담 손님 정보 조회 성공", recommendLoans);
+    }
+
 
     @Operation(summary = "대출 추천", description = "손님 정보와 매물 정보로 대출을 추천한다.")
     @ApiResponse(responseCode = "200", description = "대출 상품 추천 성공")
