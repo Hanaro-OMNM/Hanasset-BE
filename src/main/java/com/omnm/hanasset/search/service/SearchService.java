2bd708a2ec167fe160132379c0b132560517c36b
Ji-eun Yang <jidak1165@gmail.com>
2024-12-29 01:21:24 +0900

    [proj-139] fix: 주소명 응답 형태 수정

--- src/main/java/com/omnm/hanasset/search/service/SearchService.java
+++ src/main/java/com/omnm/hanasset/search/service/SearchService.java
@@ -24,14 +24,24 @@ public class SearchService {
 
         return results.map(result -> {
             Point coordinate = result.getAreaCode().getCoordinate();
-                    return SearchResponse.builder()
-                            .housingComplexId(result.getHousingComplexId())
-                            .complexName(result.getName())
-                            .addressName(result.getAddress())
-                            .lat(coordinate.getY())
-                            .lng(coordinate.getX())
-                            .build();
-                })
-                .toList();
+            StringBuilder address = new StringBuilder();
+            // 서울이면 addressName이 "금천구 가산동" 이런 형태가 되도록
+            if (result.getAreaCode().getCityCode() == 1100000000) {
+                address.append(result.getAreaCode().getSigunguName());
+                address.append(" ");
+                address.append(result.getAreaCode().getEmdName());
+            } else {
+                address.append(result.getAddress());
+            }
+
+            return SearchResponse.builder()
+                        .housingComplexId(result.getHousingComplexId())
+                        .complexName(result.getName())
+                        .addressName(address.toString())
+                        .lat(coordinate.getY())
+                        .lng(coordinate.getX())
+                    .build();
+            })
+            .toList();
     }
 }
