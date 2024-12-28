0c1f394a673614c1a17c773e217e1c6e3da1e1b6
Ji-eun Yang <jidak1165@gmail.com>
2024-12-29 00:41:07 +0900

    [proj-139] feat: QueryDSL 의존성 추가

--- src/main/java/com/omnm/hanasset/HanassetApplication.java
+++ src/main/java/com/omnm/hanasset/HanassetApplication.java
@@ -3,9 +3,11 @@ package com.omnm.hanasset;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "com.omnm.hanasset")
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HanassetApplication {
 	public static void main(String[] args) {
